28e04d0e9e6cca733cd393b6eee9010492a5a03f
Steve Xu <stevexugc@gmail.com>
2024-02-26 21:39:25 +0800

    feat: init

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+export const USER_NAME = 'Steve Xu';
+export const SITE_TITLE = `${USER_NAME} Blog`;
+export const SITE_DESCRIPTION = `Welcome to ${SITE_TITLE}!`;
+export const GITHUB_URL = 'https://github.com/nusr/nusr.github.io';
+export const GITHUB_TEXT = `Go to ${USER_NAME}'s GitHub repo`;
+export const TWITTER_URL = 'https://twitter.com/stevenusr';
+export const TWITTER_TEXT = `Follow ${USER_NAME} on Twitter`;
